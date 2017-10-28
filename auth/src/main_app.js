836b5ee7bb31436bf3d387fe0aae45a27b9c8464
jcarlos <carlosmaster10@gmail.com>
2017-10-27 23:11:36 -0300

    copy and paste reusable components from albums project, and create index.js file in common folder (to export all components). notice that each file in common not use default word in export. finally, Header component is used in MainApp component

--- auth/src/main_app.js
+++ auth/src/main_app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
 
 export default class MainApp extends Component {
   render() {
     return (
       <View>
+        <Header headerText="Authentication" />
         <Text>An App!</Text>
       </View>
     );
