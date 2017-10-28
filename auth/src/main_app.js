a683936f9c11b509c7cd14e1c55b49d00a91c5d8
jcarlos <carlosmaster10@gmail.com>
2017-10-27 22:47:51 -0300

    MainApp created

---
+++ auth/src/main_app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class MainApp extends Component {
+  render() {
+    return (
+      <View>
+        <Text>An App!</Text>
+      </View>
+    );
+  }
+}
