91384a00411421ce838a1c4d8fd5f112a966b0e8
jcarlos <carlosmaster10@gmail.com>
2017-10-27 23:52:17 -0300

    firebase added

--- auth/src/main_app.js
+++ auth/src/main_app.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import firebaseInfo from '../firebase_info.json';
 
 export default class MainApp extends Component {
+  componentWillMount() {
+    firebase.initializeApp(firebaseInfo);
+  }
+
   render() {
     return (
       <View>
