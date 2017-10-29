ab41fcad832201b58c7690ff8d798be0afeea341
jcarlos <carlosmaster10@gmail.com>
2017-10-28 23:33:49 -0300

    Input and LoginForm created

---
+++ auth/src/components/login_form.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Button, Card, CardSection, Input } from './common';
+
+export default class LoginForm extends Component {
+  state = { text: '' };
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label='Email'
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+          />
+        </CardSection>
+        <CardSection />
+        <CardSection>
+          <Button>
+            Log in
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
--- auth/src/main_app.js
+++ auth/src/main_app.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
 import firebaseInfo from '../firebase_info.json';
+import LoginForm from './components/login_form';
 
 export default class MainApp extends Component {
   componentWillMount() {
@@ -13,7 +14,7 @@ export default class MainApp extends Component {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App!</Text>
+        <LoginForm />
       </View>
     );
   }
