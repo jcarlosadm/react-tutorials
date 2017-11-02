bdab4753172874bcdfaff1be29c8aaf9abd8c3b1
jcarlos <carlosmaster10@gmail.com>
2017-11-02 01:08:57 -0300

    header added

--- tech_stack/src/main.js
+++ tech_stack/src/main.js
@@ -3,12 +3,15 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 export default class Main extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View />
+        <View>
+          <Header headerText="Tech Stack" />
+        </View>
       </Provider>
     );
   }
