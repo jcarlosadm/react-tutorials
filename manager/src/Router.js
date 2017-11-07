0e834c3effee2704c8923dfa0ebf6b4d9dff573d
jcarlos <carlosmaster10@gmail.com>
2017-11-07 01:13:53 -0300

    added features: add employee, list employee

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/login_form';
+import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
-    <Router>
-      <Scene
-        key='login'
-        component={LoginForm}
-        title="Please Login"
-      />
+    <Router sceneStyle={{ paddingTop: 65 }} >
+      <Scene key='auth'>
+        <Scene key='login' component={LoginForm} title="Please Login" />
+      </Scene>
+
+      <Scene key='main'>
+        <Scene
+          onRight={function(){Actions.employeeCreate()}}
+          rightTitle='Add'
+          key='employeeList'
+          component={EmployeeList}
+          title='Employees'
+          initial
+        />
+        <Scene key='employeeCreate' component={EmployeeCreate} title='Create Employee' />
+      </Scene>
     </Router>
   );
 };
--- manager/src/reducers/index.js
+++ manager/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer.js';
+import EmployeeFormReducer from './EmployeeFormReducer';
+import EmployeeReducer from './EmployeeReducer';
 
 export default combineReducers({
-  auth: AuthReducer
+  auth: AuthReducer,
+  employeeForm: EmployeeFormReducer,
+  employees: EmployeeReducer
 });
