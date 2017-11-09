a1931623ac1f24177c1df18b98c70ac27c9e5347
jcarlos <carlosmaster10@gmail.com>
2017-11-09 00:42:16 -0300

    manager app finished

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -58,3 +58,15 @@ export const employeeSave = ({ name, phone, shift, uid }) => {
 export const employeeReset = () => {
   return { type: EMPLOYEE_RESET };
 };
+
+export const employeeDelete = ({ uid }) => {
+  const { currentUser } = firebase.auth();
+  
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+      .remove()
+      .then(() => {
+        Actions.employeeList({ type: 'reset' });
+      });
+  };
+};
--- manager/src/components/common/Confirm.js
+++ manager/src/components/common/Confirm.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Text, View, Modal } from 'react-native';
+import { Text, View, Modal, StyleSheet } from 'react-native';
 import { CardSection } from './card_section';
 import { Button } from './button';
 
 const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  const { containerStyle, textStyle, cardSectionStyle } = styles;
+
   return (
       <Modal
         visible={visible}
@@ -11,9 +13,11 @@ const Confirm = ({ children, visible, onAccept, onDecline }) => {
         animationType='slide'
         onRequestClose={() => {}}
       >
-        <View>
-          <CardSection>
-            <Text>{children}</Text>
+        <View style={containerStyle}>
+          <CardSection style={cardSectionStyle}>
+            <Text style={textStyle}>
+              {children}
+            </Text>
           </CardSection>
 
           <CardSection>
@@ -24,5 +28,23 @@ const Confirm = ({ children, visible, onAccept, onDecline }) => {
       </Modal>
   );
 };
+
+const styles = StyleSheet.create({
+  cardSectionStyle: {
+    justifyContent: 'center'
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0,0,0,0.75)',
+    position: 'relative',
+    flex: 1,
+    justifyContent: 'center'
+  }
+});
 
 export { Confirm };
