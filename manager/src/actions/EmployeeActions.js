dc7e2675618b31741534861266ce1d54e98d34c5
jcarlos <carlosmaster10@gmail.com>
2017-11-08 23:36:09 -0300

    manager project almost finished

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -3,7 +3,9 @@ import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE,
   EMPLOYEE_CREATE,
-  EMPLOYEES_FETCH_SUCCESS
+  EMPLOYEES_FETCH_SUCCESS,
+  EMPLOYEE_SAVE_SUCCESS,
+  EMPLOYEE_RESET
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -38,4 +40,21 @@ export const employeesFetch = () => {
         });
       });
   };
+};
+
+export const employeeSave = ({ name, phone, shift, uid }) => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+      .set({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
+        Actions.employeeList({ type: 'reset' });
+      });
+  };
+};
+
+export const employeeReset = () => {
+  return { type: EMPLOYEE_RESET };
 };
